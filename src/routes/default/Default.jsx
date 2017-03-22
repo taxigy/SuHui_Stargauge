b13937b0f8d475586aadf18bef7bed2d28769ba5
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2017-03-23 00:30:09 +0100

    showing hanxi

--- src/routes/default/Default.jsx
+++ src/routes/default/Default.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
-import './Default.pcss';
+import data from '../../data';
+import styles from './Default.pcss';
 
 export default () => (
-  <div>whatev</div>
+  <div className={styles.root}>
+    {data.map((row, i) => (
+      <div
+        key={i}
+        className={styles.row}
+      >
+        {row.map((col, j) => (
+          <div
+            key={j}
+            className={[styles.col, styles.col_fitVertical].join(' ')}
+          >
+            {col}
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
 );
